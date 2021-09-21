f2140fefad22534c4e5f350b4be73e943195816f
Christian Estela <cestela3@gmail.com>
2021-09-21 19:11:29 -0400

    Initial commit

---
+++ src/EmployeeListRemoval.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class EmployeeListRemoval {
+    public static void main(String[] args) {
+
+        ArrayList<String> employees = new ArrayList<String>(Arrays.asList("Joel Jackson","Jack Jones","Chris Smith","Amanda Goodman","Jeremy Lee"));
+        printEmployees(employees);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("Enter an employee name to be removed: ");
+        String nameToRemove = sc.nextLine();
+
+        employees.remove(nameToRemove);
+
+        System.out.println();
+        printEmployees(employees);
+    }
+    public static void printEmployees(ArrayList<String> employees)
+    {
+        System.out.println("There are "+ employees.size() +" employees: ");
+        for(String emp:employees) {
+            System.out.println(emp);
+        }
+    }
+}
+
